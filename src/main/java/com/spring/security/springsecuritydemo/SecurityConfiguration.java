313e14a5a5f9d5493d4f8a5c42fee50833b3c331
Lavish Yadav <lavishyadav@live.com>
2020-05-09 14:16:57 +0530

    Enabled web security

---
+++ src/main/java/com/spring/security/springsecuritydemo/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.spring.security.springsecuritydemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        //Set config of auth object
+        managerBuilder.inMemoryAuthentication().withUser("user").password("pass").roles("ROLE");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
