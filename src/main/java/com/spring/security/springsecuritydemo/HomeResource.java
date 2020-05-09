4a59901e91eeb793e777104e473223baa274e727
Lavish Yadav <lavishyadav@live.com>
2020-05-09 14:35:36 +0530

    configured HttpSecurity

--- src/main/java/com/spring/security/springsecuritydemo/HomeResource.java
+++ src/main/java/com/spring/security/springsecuritydemo/HomeResource.java
@@ -2,12 +2,21 @@ package com.spring.security.springsecuritydemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeResource {
     
-    @GetMapping("/home")
+    @GetMapping("/")
     public String defaultRoute(){
         return "Hello";
+    }
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
     }
 }
--- src/main/java/com/spring/security/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/spring/security/springsecuritydemo/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.spring.security.springsecuritydemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,11 +13,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
         //Set config of auth object
-        managerBuilder.inMemoryAuthentication().withUser("user").password("pass").roles("ROLE").and()
-         .withUser("user2").password("pass").roles("ROLE");
+        managerBuilder.inMemoryAuthentication().withUser("user").password("pass").roles("USER").and()
+         .withUser("admin").password("pass").roles("ADMIN");
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN","USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+                
     }
 }
